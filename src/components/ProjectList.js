70406b9ea7523e0e05562cc894c82637d764813c
Donte Ladatto <donte279@gmail.com>
2022-04-09 13:10:26 -0500

    all tests passing

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,15 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const pComponents = projects.map(obj => {
+    return (
+    <ProjectItem key={obj.id} name={obj.name} about={obj.about} technologies={obj.technologies} />
+    )
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{pComponents}</div>
     </div>
   );
 }
